0a91cea318a0b6ce9ac6d2be4f228a94620a3bf8
InKyungWoo <chelsea1201@naver.com>
2024-07-15 12:47:07 +0900

    :sparkles: feat: Splash 화면 구현

--- src/pages/Splash.jsx
+++ src/pages/Splash.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
-const logoImg = require('../assets/wod_logo.png');
+const logoImg = require('../assets/splash_logo.png');
 
 const Splash = ({ navigation }) => {
     useEffect(() => {
